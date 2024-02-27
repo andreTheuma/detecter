bca24d645129e2410be5e876efa79a227fe258e7
andreTheuma <andre.theuma@gmail.com>
2024-02-27 01:06:10 +0100

    Initial implementation of single event regeneration

---
+++ detecter/src/regeneration/event_table_parser.erl
@@ -0,0 +1,79 @@
+-module(event_table_parser).
+-author("André Theuma").
+
+-compile([debug_info]).
+
+-export([parse_table/1]).
+-export([string_tokenizer/1]).
+-export([file_format_checker/1]).
+
+-export_type([event_table/0, event_atom/0]).
+
+-import(event, [evm_event/0]).
+
+-type event_table() :: [event_relation()].
+-type event_relation() :: {event_atom(), event_atom(), event_atom()}.
+-type event_atom() :: atom().
+
+%% @doc The entry function to parse the event table.
+-spec parse_table(Filename) -> event_table() when Filename :: string().
+parse_table(Filename) ->
+    case file:open(Filename, [read]) of
+        {ok, _} ->
+            {ok, File} = file:read_file(Filename),
+            Lines = string:tokens(binary_to_list(File), "\n"),
+            case file_format_checker(Lines) of
+                ok ->
+                    EventTable = lists:map(fun string_tokenizer/1, Lines),
+                    {ok, EventTable};
+                {error, Error} ->
+                    {error, Error}
+            end;
+        {error, Error} ->
+            {error, Error}
+    end.
+
+%% @doc Tokenizes a string into a list of strings. The string takes the form of
+%% <current_event>,[{previous_events},{next_events}] where <current_event> is the
+%% current event, {previous_events} is a list of previous events in the form of {event1,event2,...} and {next_events} is a list of next events in the form of {event1,event2,...}
+-spec string_tokenizer(Line) -> [event_relation()] when Line :: string().
+string_tokenizer(Line) ->
+    % Clean the line from any whitespace
+    CleanLine = re:replace(Line, "\\s+", "", [global, {return, list}]),
+
+    % Split the line into the current event and the rest of the events
+    [CurrentEvent | Rest] = string:lexemes(CleanLine, "|"),
+    % Clean the rest of the events from { and }
+    CleanRest = re:replace(Rest, "[{}]", "", [global, {return, list}]),
+
+    % Split the rest of the events into the previous and next events
+    [PreviousEventsStrBin | NextEventsStrBin] = re:split(CleanRest, ";", [{parts, 2}]),
+
+    PreviousEvents = string:tokens(binary_to_list(PreviousEventsStrBin), ","),
+    NextEvents = string:tokens(binary_to_list(hd(lists:flatten(NextEventsStrBin))), ","),
+
+    {CurrentEvent, PreviousEvents, NextEvents}.
+
+
+-spec file_format_checker(Lines) -> [ok | {error, string()}] when Lines :: [string()].
+file_format_checker(Lines)->
+    RegEx = "^[A-Za-z0-9]+ \\| \\{[^}]*\\}; \\{[^}]*\\}$",
+    case lists:all(fun(Line) ->
+        % if line starts with a comment, ignore it
+        case re:run(Line, "^\\s*%") of
+            nomatch -> ok;
+            _ -> return
+        end,
+
+        case re:run(Line, RegEx) of
+            nomatch ->
+                io:format("Line ~p does not match the format.~n", [Line]),
+                false;
+            {match, _} -> 
+                true
+        end
+    end, Lines) of
+        true -> ok;
+        false -> {error, "Invalid format. The format should be <current_event> | {<previous_events>}; {<next_events>"}
+    end.
+    
---
+++ detecter/src/regeneration/regen_eval.erl
@@ -0,0 +1,108 @@
+-module(regen_eval).
+-author("André Theuma").
+
+-compile([debug_info]).
+
+-export([event_exists/1]).
+-export([get_previous_events/2, get_next_events/2]).
+-export([generate_missing_event/3]).
+
+-export([test/3]).
+
+%% @doc Reads an event from the trace and checks if it exists ie: when mfa is not 'undefined'. if it is undefined,
+%% return false, else return true.
+-spec event_exists(Event) -> boolean() 
+    when 
+    Event :: event:evm_event().
+event_exists(Event) ->
+    case Event of
+        {trace,_,spawn,_,Mfa} when Mfa =:= undefined -> 
+            true;
+        {trace,_,spawned,_,Mfa} when Mfa =:= undefined ->
+            true;
+        {trace,_,exit,_}-> 
+            true;
+        {trace,_,send,_,_} ->
+            true;
+        {trace,_,'receive',_} ->
+            true;
+        _ ->
+            false
+    end.
+
+%% @doc Returns the previous events of the current event by checking the event table and returning the previous events 
+-spec get_previous_events(CurrentEvent, EventTable) -> [event:event_atom()]
+    when
+    CurrentEvent :: event_table_parser:event_atom(),
+    EventTable :: event_table_parser:event_table().
+get_previous_events(_, []) ->
+    [];
+get_previous_events(CurrentEvent, [{Event, PreviousEvents, _} | Rest]) ->
+    case CurrentEvent =:= Event of
+        true ->
+            PreviousEvents;
+        false ->
+            get_previous_events(CurrentEvent, Rest)
+    end.
+
+-spec get_next_events(CurrentEvent, EventTable) -> [event:event_atom()]
+    when
+    CurrentEvent :: event_table_parser:event_atom(),
+    EventTable :: event_table_parser:event_table().
+get_next_events(_, []) ->
+    [];
+get_next_events(CurrentEvent, [{Event, _, NextEvents} | Rest]) ->
+    case CurrentEvent =:= Event of
+        true ->
+            NextEvents;
+        false ->
+            get_next_events(CurrentEvent, Rest)
+    end.
+
+-spec generate_missing_event(NextEvent,PreviousEvent, EventTable) -> [event:event_atom()]
+    when
+    NextEvent :: event_table_parser:event_atom(),
+    PreviousEvent :: event_table_parser:event_atom(),
+    EventTable :: event_table_parser:event_table().
+generate_missing_event(NextEvent,PreviousEvent, EventTable)->
+
+    PreviousEvents = get_previous_events(NextEvent, EventTable),
+    NextEvents = get_next_events(PreviousEvent, EventTable),
+
+    case {PreviousEvent, NextEvent} of
+        {_,_} when length(PreviousEvents) > 0, length(NextEvents) > 0 ->
+            Intersection = ordsets:intersection(ordsets:from_list(PreviousEvents), ordsets:from_list(NextEvents)),
+            case length(Intersection) of
+                1->
+                    {ok,hd(Intersection)};
+                0 ->
+                    undefined;
+                _ -> 
+                    {error,"Error: More than one event in common between previous and next events."}
+            end;
+        _ ->
+            undefined
+    end.
+
+
+%% TESTS
+
+test(PreviousEvent, NextEvent, FileName) ->
+    case event_table_parser:parse_table(FileName) of
+        {ok, Table} -> 
+            io:format("Table is ~p~n", [Table]),
+            % PreviousEvents = get_previous_events(CurrentEvent, Table),
+            % io:format("Previous events of ~p are ~p~n", [CurrentEvent, PreviousEvents]);
+            case generate_missing_event(NextEvent,PreviousEvent, Table) of
+                undefined ->
+                    io:format("No missing event found.~n");
+                {ok, MissingEvent} ->
+                    io:format("Missing event is ~p~n", [MissingEvent]);
+                {error, Error} ->
+                    io:format("Error: ~p~n", [Error])
+            end;
+        {error, Error} -> 
+            io:format("Error: ~p~n", [Error])
+    end.
+
+
